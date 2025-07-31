61f4ab90d8f730577a3320707469989f54e1cbcc
Adrián Chantreux Fermoso <chantreuxa@predictia.es>
2025-07-31 17:22:09 +0200

    sum to mean

--- standardization/derived-era5-single-levels-daily-statistics.py
+++ standardization/derived-era5-single-levels-daily-statistics.py
@@ -6,7 +6,7 @@ def tp_mm(ds, var):
     	if var == data_var:
             ds[var].attrs['standard_name'] = "precipitation_flux"
             ds[var].attrs['units'] = "kg m-2 s-1"
-            ds[var].attrs['cell_methods'] = "area: time: sum"
+            ds[var].attrs['cell_methods'] = "area: time: mean"
             ds[var].attrs['long_name'] = "Precipitation"
             ds[var].attrs['comment'] = "includes both liquid and solid phases"    		
             ds[var] = ds[var]/86400
@@ -21,7 +21,7 @@ def tp_m(ds, var):
     	if var == data_var:
             ds[var].attrs['standard_name'] = "precipitation_flux"
             ds[var].attrs['units'] = "kg m-2 s-1"
-            ds[var].attrs['cell_methods'] = "area: time: sum"
+            ds[var].attrs['cell_methods'] = "area: time: mean"
             ds[var].attrs['long_name'] = "Precipitation"
             ds[var].attrs['comment'] = "includes both liquid and solid phases"    		
             ds[var] = ds[var]*1000/86400
@@ -37,7 +37,7 @@ def e_m(ds, var):
     	if var == data_var:
             ds[var].attrs['standard_name'] = "surface_water_evaporation_flux"
             ds[var].attrs['units'] = "kg m-2 s-1"
-            ds[var].attrs['cell_methods'] = "area: time: sum"
+            ds[var].attrs['cell_methods'] = "area: time: mean"
             ds[var].attrs['long_name'] = "Evaporation"
             ds[var].attrs['comment'] = "negative values indicate evaporation and positive values indicate condensation"    		
             ds[var] = ds[var]*1000/86400
@@ -52,7 +52,7 @@ def ssrd(ds, var):
     	if var == data_var:
             ds[var].attrs['standard_name'] = "surface_downwelling_shortwave_flux_in_air"
             ds[var].attrs['units'] = "J m-2"
-            ds[var].attrs['cell_methods'] = "area: time: sum"
+            ds[var].attrs['cell_methods'] = "area: time: mean"
             ds[var].attrs['long_name'] = "Surface short-wave (solar) radiation downwards"  		
             ds[var] = ds[var]/ 3600
     return ds
